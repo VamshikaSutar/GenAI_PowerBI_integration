2476b4580cabea70b82cd4651c72399761aa431a
VamshikaSutar <96682861+VamshikaSutar@users.noreply.github.com>
2025-08-05 09:08:25 +0530

    Update client_manager.py

--- client_manager.py
+++ client_manager.py
@@ -11,6 +11,11 @@ class ClientManager:
 
     def get_excel_path(self):
         return self.excel_path
+    
+    def get_powerbi_url(self):
+        return "https://app.powerbi.com/reportEmbed?reportId=c3c44ac7-7ebe-4ec5-9327-5e0557729e07&autoAuth=true&ctid=5989ece0-f90e-40bf-9c79-1a7beccdb861"
 
-    # def get_powerbi_url(self):
+    
+    # PowerBI sample url1 - "https://app.powerbi.com/reportEmbed?reportId=c3c44ac7-7ebe-4ec5-9327-5e0557729e07&autoAuth=true&ctid=5989ece0-f90e-40bf-9c79-1a7beccdb861"
+    # PowerBI sample iframe URL1 - "<iframe title="PBI_testing1" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=c3c44ac7-7ebe-4ec5-9327-5e0557729e07&autoAuth=true&ctid=5989ece0-f90e-40bf-9c79-1a7beccdb861" frameborder="0" allowFullScreen="true"></iframe>"
     #     return self.powerbi_url
